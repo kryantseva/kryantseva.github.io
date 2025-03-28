29ff1a861ce16c058d2c54b90f66c01c80ba80ee
kryantseva <134473671+kryantseva@users.noreply.github.com>
2025-03-28 19:26:33 +0300

    Create task-component.js

---
+++ src/view/task-component.js
@@ -0,0 +1,26 @@
+import { createElement } from '../framework/render.js';
+
+function createTaskTemplate(taskText) {
+  return `<div class="task">${taskText}</div>`;
+}
+
+export default class TaskComponent {
+  constructor(taskText) {
+    this.taskText = taskText;
+  }
+
+  getTemplate() {
+    return createTaskTemplate(this.taskText);
+  }
+
+  getElement() {
+    if (!this.element) {
+      this.element = createElement(this.getTemplate());
+    }
+    return this.element;
+  }
+
+  removeElement() {
+    this.element = null;
+  }
+}
